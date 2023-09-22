bf6574cabd3461c0f504455ab3df069b6e7d0311
Vitor Wilson <vitorwilson@tre-ce.jus.br>
2023-09-22 10:27:51 -0300

    melhorias na interface frontend

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_jwt_extended import JWTManager
-
+import requests as req
 from datetime import timedelta
 from os import getenv as env
+from services.Maps_api import Maps_api
+import googlemaps
 
 from routes.Weather_api_routes import weather_api_routes
 from routes.Maps_api_routes import maps_api_routes
 from routes.Autentication_routes import autentication_routes
+
+gmaps = googlemaps.Client(key=env("MAPS_KEY"))
 
 app = Flask(__name__, template_folder='views', static_folder='static')
 
@@ -31,6 +35,38 @@ app.register_blueprint(weather_api_routes, url_prefix='/weather')
 @app.route("/contato", methods=['GET'])
 async def contato():
     return render_template('pages/contato.html', title='Contato')
+
+
+lugares = []
+@app.route("/places", methods=['GET', "POST"])
+async def places():
+    if request.method == 'POST':
+        lugares.clear()
+        location_address = request.form.get("location")
+        radius = request.form.get("radius")
+
+        if not location_address or not radius:
+            return redirect(url_for('places'))
+        
+        location_result = gmaps.geocode(location_address)[0].get("geometry").get("location")
+        location_lat_lng = f"{location_result.get('lat')},{location_result.get('lng')}"
+        maps_api = Maps_api()
+
+        brute_places = maps_api.get_places(location_lat_lng, radius)
+
+        for place in brute_places.get("results"):
+            if place.get("opening_hours") and place.get("opening_hours").get("open_now"):
+                lugares.append({
+                    "name": place.get("name"),
+                    "address": place.get("vicinity"),
+                    "rating": place.get("rating"),
+                    "types": place.get("types"),
+                    "location": place.get("geometry").get("location"),
+                    "open_now": place.get("opening_hours").get("open_now")
+            })
+        return redirect(url_for('places'))
+    elif request.method == 'GET':
+        return render_template('pages/places.html', title='Lugares', lugares=lugares)
 
 @app.route('/', methods=['GET'])
 async def index():
